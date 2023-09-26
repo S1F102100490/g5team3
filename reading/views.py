51a398d2eb06375f033c765cc03b1332b2503f0b
S.Kinoshita <85732349+S1F102100490@users.noreply.github.com>
2023-09-27 00:38:11 +0900

    aaaa

--- reading/views.py
+++ reading/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'reading/Reading.html')
+    return render(request, 'reading/Home.html')
 
 def reading(request):
     return render(request, 'reading/Reading.html')
 
 def freetalk(request):
-    return render(request, 'reading/Reading.html')
+    return render(request, 'reading/FreeTalk.html')
