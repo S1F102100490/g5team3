78a7afca5ab3b17d5c6adeabc5d33c86a0395b51
s1f102103314 <85728209+s1f102103314@users.noreply.github.com>
2023-09-27 15:45:50 +0900

    freetalkのページの修正

--- freetalk/views.py
+++ freetalk/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.http import Http404
+from django.utils import timezone
+from eng_app.models import Question
+from eng_app.models import Answer
+from django.contrib import messages
+from django.db.models import Q
+
 
 def freetalk(request):
     return render(request, 'freetalk/FreeTalk.html')
