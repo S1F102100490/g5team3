2a2096c99b88c07922294dc2cd73b4506ad1f624
S.Kinoshita <85732349+S1F102100490@users.noreply.github.com>
2023-11-28 17:27:20 +0900

    APIkey変更

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ import os
 import openai
 
 openai.api_base = 'https://api.openai.iniad.org/api/v1'
-openai.api_key = 'E2Ld8myvbQiLAKdOYdd-V_LV34JqlDy8aZifCjAyB_h0zhQ-vgJn7dfq_2qLq4RvOChvnhCEH57lv_x8Ks58BrQ'
+openai.api_key = 'JEASLG20yKVDNCKvsosyuMcH-u5MRvkH2CUJ1LgxcyYR2VIkoRAJaJ3iGGfWLWStWIScV3-4q4p3vSGFXI0IwTw '
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
