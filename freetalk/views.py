42b1902f187dac7fb4ee0903582cfc1b8766891e
S.Kinoshita <85732349+S1F102100490@users.noreply.github.com>
2023-11-28 17:50:07 +0900

    views.pyをfreetalk仕様に

--- freetalk/views.py
+++ freetalk/views.py
@@ -1,4 +1,4 @@
-from django.http.response import JsonResponse
+""" from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.http import Http404
@@ -6,8 +6,38 @@ from django.utils import timezone
 from eng_app.models import Question
 from eng_app.models import Answer
 from django.contrib import messages
-from django.db.models import Q
+from django.db.models import Q """
+
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import openai
 
 
 def freetalk(request):
     return render(request, 'freetalk/FreeTalk.html')
+
+
+
+
+def chatgpt(request):
+    if request.method == 'POST':
+        question = request.POST.get('question', '')
+
+        # ChatGPTとの対話
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": question},
+            ]
+        )
+
+        answer = response['choices'][0]['message']['content']
+
+        # 応答をテンプレートに渡して表示
+        return render(request, 'freetalk/FreeTalk.html', {'question': question, 'answer': answer})
+
+    return render(request, 'freetalk/FreeTalk.html')
+
