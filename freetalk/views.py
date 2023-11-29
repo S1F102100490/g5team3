903bc61ed7053b36f8b1a06e6af5d57ee9a15d00
S.Kinoshita <85732349+S1F102100490@users.noreply.github.com>
2023-11-29 14:56:33 +0900

    URL変更、増築

--- freetalk/views.py
+++ freetalk/views.py
@@ -1,14 +1,3 @@
-""" from django.http.response import JsonResponse
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
-from django.http import Http404
-from django.utils import timezone
-from eng_app.models import Question
-from eng_app.models import Answer
-from django.contrib import messages
-from django.db.models import Q """
-
-
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -17,9 +6,6 @@ import openai
 
 def freetalk(request):
     return render(request, 'freetalk/FreeTalk.html')
-
-
-
 
 def chatgpt(request):
     if request.method == 'POST':
--- sample/views.py
+++ sample/views.py
@@ -1,5 +1,3 @@
-""" views.py """
-
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
