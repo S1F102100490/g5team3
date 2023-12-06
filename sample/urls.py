db52ac25f8c4fa4feeff5190bdd58feb24c61ff4
S.Kinoshita <85732349+S1F102100490@users.noreply.github.com>
2023-12-06 15:26:33 +0900

    reading機能開発１

--- sample/urls.py
+++ sample/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import generate_text
 
 urlpatterns = [
     path('', views.chatgpt, name='sample'),
     path('ask/', views.chatgpt, name='AskGPT'),
+    path('generate_text/', views.chatgpt, name='generate_text'),
+    path('reading/', views.chatgpt, name='reading'),
 ]
--- sample/views.py
+++ sample/views.py
@@ -2,10 +2,50 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import openai
+from .models import GeneratedText
 
 def sample(request):
     return render(request, 'sample\chatgpt.html')
 
+def chatgpt(request):
+    if request.method == 'POST':
+        question = request.POST.get('question', '')
+
+        # ChatGPTとの対話
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": question},
+            ]
+        )
+
+        answer = response['choices'][0]['message']['content']
+
+        # 応答をテンプレートに渡して表示
+        return render(request, 'sample/chatgpt.html', {'question': question, 'answer': answer})
+
+    return render(request, 'sample/chatgpt.html')
+
+
+def generate_text(request):
+    if request.method == 'POST':
+        length = request.POST.get('length')
+        genre = request.POST.get('genre')
+
+        # ここでChatGPTとの対話を行い、文章を生成する処理を実装
+
+        # 生成された文章をデータベースに保存
+        generated_text = "Generated text"  # 仮の生成テキスト
+        user_input = f"Length: {length}, Genre: {genre}"  # 仮のユーザー入力
+        GeneratedText.objects.create(user_input=user_input, generated_text=generated_text)
+
+        # 生成された文章をJSON形式で返す（適切な形式に変更）
+        return JsonResponse({'generated_text': generated_text})
+
+    return render(request, 'sample/chatgpt.html')
+""" 
+# sample/views.py
 
 logs = []  # 会話のログを保持するリスト
 
@@ -61,3 +101,4 @@ def generate_article(request):
         form = ArticleForm()
 
     return render(request, 'sample/chatgpt.html', {'form': form, 'article': generated_article})
+ """
