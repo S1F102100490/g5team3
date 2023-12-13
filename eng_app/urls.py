da30a9bea8fe48455c4f455c9cff538134c32d65
s1f10210293 <85635699+s1f10210293@users.noreply.github.com>
2023-12-13 13:15:35 +0900

    a

--- eng_app/urls.py
+++ eng_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import signup_view, login_view, logout_view
 
 urlpatterns = [
 	path('', views.index, name='index'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     
 	]
--- eng_app/views.py
+++ eng_app/views.py
@@ -21,4 +21,33 @@ def index(request):
 	}
 	return render(request, 'eng_app/index.html',context)
 
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('your_home_view')  # ユーザーが登録したらホームページにリダイレクト
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('your_home_view')  # ログインしたらホームページにリダイレクト
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('your_logout_success_view')  # ログアウトしたら成功ページにリダイレクト
