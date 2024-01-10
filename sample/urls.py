d45d11c6471c1e7493dd6c0020b7c8b6a58308ad
S.Kinoshita <85732349+S1F102100490@users.noreply.github.com>
2024-01-10 15:18:24 +0900

    sampleの上書き

--- sample/urls.py
+++ sample/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import generate_text
+from .views import reading
 
 urlpatterns = [
     path('', views.chatgpt, name='sample'),
     path('ask/', views.chatgpt, name='AskGPT'),
+    path('generate_text/', views.chatgpt, name='generate_text'),
+    path('reading/', views.reading, name='reading'),
 ]
