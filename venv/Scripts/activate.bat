6ef0975857f2d7a2106ec1c4c1846f6358827dee
S.Kinoshita <85732349+S1F102100490@users.noreply.github.com>
2024-01-13 01:24:11 +0900

    Revert "1/13に自分の環境が動くように設定したやつ"
    This reverts commit 20d0f7c5ef6e777c31e166f9d221b70c49fb3631.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\s1F10\Documents\GitHub\g5team3\venv
+set VIRTUAL_ENV=C:\Users\iniad\g5team3\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
